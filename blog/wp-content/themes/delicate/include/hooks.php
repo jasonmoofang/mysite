6918a7aca5855ce5238e90153d7dde890f710606
Lim Yuen Hoe <yuenhoe@hotmail.com>
2013-05-18 01:24:18 -0700

    change default custom baackground behavior in theme - don't immediately insert background color, set it with javascript onload. This so text won't be invisible in the beginning

--- blog/wp-content/themes/delicate/include/hooks.php
+++ blog/wp-content/themes/delicate/include/hooks.php
@@ -8,7 +8,50 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'slide-thumb', 970, 225, true); //(cropped)
 }
 
-add_custom_background(); // Add support for custom backgrounds
+function yh_custom_background() {
+        // $background is the saved custom image, or the default image.
+        $background = get_background_image();
+
+        // $color is the saved custom color.
+        // A default has to be specified in style.css. It will not be printed here.
+        $color = get_theme_mod( 'background_color' );
+
+        if ( ! $background && ! $color )
+                return;
+        $style = "";
+        if ($color) {
+?>
+            <script>window.onload=function() { document.body.style.backgroundColor='#<?php echo $color ?>'; };</script>
+<?php
+        }
+
+        if ( $background ) {
+                $image = " background-image: url('$background');";
+
+                $repeat = get_theme_mod( 'background_repeat', 'repeat' );
+                if ( ! in_array( $repeat, array( 'no-repeat', 'repeat-x', 'repeat-y', 'repeat' ) ) )
+                        $repeat = 'repeat';
+                $repeat = " background-repeat: $repeat;";
+
+                $position = get_theme_mod( 'background_position_x', 'left' );
+                if ( ! in_array( $position, array( 'center', 'right', 'left' ) ) )
+                        $position = 'left';
+                $position = " background-position: top $position;";
+
+                $attachment = get_theme_mod( 'background_attachment', 'scroll' );
+                if ( ! in_array( $attachment, array( 'fixed', 'scroll' ) ) )
+                        $attachment = 'scroll';
+                $attachment = " background-attachment: $attachment;";
+
+                $style .= $image . $repeat . $position . $attachment;
+        }
+?>
+<style type="text/css" id="custom-background-css">
+body.custom-background { <?php echo trim( $style ); ?> }
+</style>
+<?php
+}
+add_custom_background('yh_custom_background'); // Add support for custom backgrounds
 
 // Custom Header
 define( 'HEADER_TEXTCOLOR', '333333' );
